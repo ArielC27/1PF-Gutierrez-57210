a95c16678a4bd2db96386e11595275beb54616f0
Ariel Gutierrez <agutierrez@gmssa.com.ar>
2024-07-15 20:28:22 -0300

    Add  dataSource and displayedColumns to alumnos

--- src/app/pages/alumnos/alumnos.component.ts
+++ src/app/pages/alumnos/alumnos.component.ts
@@ -10,6 +10,34 @@ import { Alumno } from '../../core/models/alumno';
 export class AlumnosComponent implements OnInit {
   alumnos: Alumno[] = [];
 
+  displayedColumns: string[] = [
+    'Nombre',
+    'Apellido',
+    'Edad',
+    'Domicilio',
+    'Telefono',
+    'Email',
+  ];
+  dataSource: Alumno[] = [
+    {
+      Nombre: 'Juan',
+      Apellido: 'Pérez',
+      Edad: '20',
+      Domicilio: 'Calle Falsa 123',
+      Telefono: '123456789',
+      Email: 'juan@example.com',
+    },
+    {
+      Nombre: 'María',
+      Apellido: 'González',
+      Edad: '22',
+      Domicilio: 'Avenida Siempre Viva 456',
+      Telefono: '987654321',
+      Email: 'maria@example.com',
+    },
+    // otros alumnos
+  ];
+
   constructor(private alumnoService: AlumnoService) {}
 
   ngOnInit(): void {
